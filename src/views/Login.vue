c2473b7c5d7377e89b5d2c327dfc7b1162e29c69
fange123 <2030564240@qq.com>
2021-10-20 17:55:41 +0800

    dev:添加登录页面

---
+++ src/views/Login.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+      <el-form ref='loginForm' :model='loginForm' class="login_container" :rules="rules">
+        <h3 class="login_title">系统登录</h3>
+        <el-form-item prop='username'>
+          <el-input type='text'  v-model='loginForm.username' placeholder='请输入用户名' autocomplete="false"></el-input>
+        </el-form-item>
+        <el-form-item prop='password'>
+          <el-input type='password' v-model='loginForm.password' placeholder='请输入密码' autocomplete="false"></el-input>
+        </el-form-item>
+        <el-form-item prop='code'>
+          <el-input  v-model='loginForm.code' placeholder='点击图片更换验证码' autocomplete="false" type='text' style="width:250px;margin-right:5px"></el-input>
+        <img :src="captchaUrl" alt="">
+
+
+        </el-form-item>
+        <el-checkbox v-model='checked' class="login_remember">记住我</el-checkbox>
+        <el-button type='primary' style="width:100%" @click='submitLogin'>登录</el-button>
+
+      </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  name:'Login',
+  data(){
+    return{
+      captchaUrl:'',
+      loginForm :{
+        username:'admin',
+        password:'123',
+        code:''
+      },
+      checked:true,
+      rules: {
+          username: [
+            { required: true, message: '请输入用户名称', trigger: 'blur' },
+            { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'change' }
+          ],
+          code: [
+            { required: true, message: '请输入验证码', trigger: 'change' }
+          ],
+
+
+        }
+
+    }
+  },
+  methods:{
+    submitLogin(){
+      this.$refs.loginForm.validate((valid) => {
+          if (valid) {
+            this.$message.success('success')
+          } else {
+            this.$message.error('请填写所有字段')
+            return false;
+          }
+        });
+    }
+  },
+
+    components: {
+
+    },
+};
+</script>
+
+<style>
+.login_container {
+  width: 350px;
+  margin: 180px auto;
+  padding: 15px 35px 20px 35px;
+  box-shadow: 0 0 25px #c6c6ca;
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+}
+.login_title {
+  margin: 0;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.login_remember {
+  margin: 0 5px 20px 0;
+}
+
+</style>
